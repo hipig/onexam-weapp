e86e0c2507cd782074bac27a795286580e0f59e5
pigzzz123 <205270006@qq.com>
2021-11-03 00:00:51 +0800

    fixed

--- src/components/question/Item.vue
+++ src/components/question/Item.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="px-5">
-    <view>
+    <view class="mb-2">
       <view class="py-3">
         <view class="inline-block mr-1">
           <view class="px-1 text-xs rounded-sm" :class="[typeClasses]">{{ typeText }}</view>
@@ -24,17 +24,25 @@
       </view>
     </view>
     <view class="duration-300 ease-in-out" :class="[isRecite || isAnswered || isShowAnswer ? 'opacity-100' : 'opacity-0']">
-      <view class="py-4">
-        <text class="text-lg" :class="[checkCorrectOption(answer) ? 'text-green-500' : 'text-red-500']" v-if="isAnswered">{{ checkCorrectOption(answer) ? '回答正确' : '回答错误' }}</text>
-      </view>
-      <view class="py-1 flex">
-        <view class="mr-3">答案</view>
-        <view class="flex-1 min-w-0">
-          <text class="text-green-500">{{ correctAnswer }}</text>
+      <view class="py-2">
+        <view class="py-2 px-3 bg-gray-100 rounded">
+          <view class="text-lg" :class="[checkCorrectOption(answer) ? 'text-green-500' : 'text-red-500']" v-if="isAnswered">{{ checkCorrectOption(answer) ? '回答正确' : '回答错误' }}</view>
+          <view class="py-1 flex mt-1">
+            <view class="mr-4">答案</view>
+            <view class="flex-1 min-w-0">
+              <text class="text-green-500 font-bold">{{ correctAnswer }}</text>
+            </view>
+          </view>
+          <view class="flex mt-1" v-if="!checkCorrectOption(answer) && isAnswered">
+            <view class="mr-4">你的答案</view>
+            <view class="flex-1 min-w-0">
+              <text class="text-gray-900 font-bold">{{ answer }}</text>
+            </view>
+          </view>
         </view>
       </view>
       <view class="py-3 flex">
-        <view class="mr-3">解析</view>
+        <view class="mr-4 text-gray-500">解析</view>
         <view class="flex-1 min-w-0">
           <view v-html="parse"></view>
         </view>
@@ -65,7 +73,8 @@ export default {
   data() {
     return {
       checkIcon,
-      xIcon
+      xIcon,
+      optionsClasses: {}
     }
   },
   props: {
@@ -75,7 +84,7 @@ export default {
       default: 1
     },
     options: Object | null,
-    optionsClasses: Object | null,
+    // optionsClasses: Object | null,
     correctAnswer: {
       type: String | Array,
       default: ''
@@ -108,11 +117,30 @@ export default {
       return questionTypeMap[this.questionType].text
     }
   },
+  watch: {
+    isAnswered(val) {
+      if (val) {
+        this.calculateOptionClass()
+      }
+    }
+  },
   methods: {
     handleAnswer(key) {
       if (!this.isRecite && !this.isAnswered && !this.isShowAnswer) {
         eventCenter.trigger('on.answer', key)
       }
+    },
+    calculateOptionClass() {
+      _.forEach(this.options, (_, key) => {
+        let classes = ''
+        if (this.checkCorrectOption(key)) {
+          classes = 'bg-green-500 border-green-500'
+        }
+        if (this.checkInCorrectOption(key)) {
+          classes = 'bg-red-500 border-red-500'
+        }
+        this.$set(this.optionsClasses, key, classes)
+      })
     },
     checkCorrectOption(key) {
       switch(this.questionType) {
--- src/pages/start/index.vue
+++ src/pages/start/index.vue
@@ -13,10 +13,9 @@
       <swiper :current="currentIndex" class="flex-1" @change="handleChange">
         <swiper-item class="h-full" v-for="(question, index) in questionList" :key="index">
           <scroll-view :scroll-y="true" class="h-inherit">
-            <question-item :title="question.title" 
+            <question-item :title="question.title"
               :question-type="question.type"
               :options="question.options"
-              :options-classes="question.optionsClasses || {}"
               :correct-answer="question.answer"
               :answer="question.selfAnswer"
               :is-answered="question.isAnswered"
@@ -28,6 +27,37 @@
       </swiper>
     </view>
     <exercise-toolbar :is-answered="currentQuestion.isAnswered" :is-show-answer="currentQuestion.isShowAnswer"></exercise-toolbar>
+    <view class="fixed inset-0 flex items-end overflow-hidden duration-500 z-50" :class="[isShowTab ? 'visible' : 'invisible']">
+      <view class="absolute inset-0 bg-gray-900 bg-opacity-50 duration-500 ease-in-out" :class="[isShowTab ? 'opacity-100' : 'opacity-0']" @tap="isShowTab = false"></view>
+      <view class="w-full h-4__5 px-5 bg-white rounded-t-3xl z-10 transform transition duration-700 ease-in-out" :class="[isShowTab ? 'translate-y-0' : 'translate-y-full']">
+        <view class="flex flex-col h-full">
+          <view class="py-4 flex items-center justify-between border-0 border-b border-solid border-gray-100">
+            <text>选项卡</text>
+            <view class="leading-none" @tap="isShowTab = false">
+              <image :src="closeIcon" class="block h-5 w-5" />
+            </view>
+          </view>
+          <view class="flex-1 min-h-0">
+            <view class="h-full">
+              <scroll-view :scroll-y="true" class="h-inherit">
+                <view class="py-3">
+                  <view class="mb-4">
+                    <view class="text-sm text-gray-900 hidden">单项选择(本类题共60题,每小题1分,共60分。每小题的备选项中，只有一个符合题意，多选，错选,不选均不得分)</view>
+                    <view class="py-3 grid grid-cols-5 gap-9">
+                      <view class="border-2 border-solid rounded-2xl overflow-hidden" v-for="(question, index) in questionList" :key="index" :class="[currentIndex == index ? 'border-gray-900' : 'border-gray-200', question.isAnswered ? 'bg-gray-200' : '']" @tap="handleToIndex(index)">
+                        <view class="w-full pb-full relative">
+                          <view class="absolute inset-0 flex items-center justify-center text-gray-500">{{ index+1 }}</view>
+                        </view>
+                      </view>
+                    </view>
+                  </view>
+                </view>
+              </scroll-view>
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -40,11 +70,15 @@ import questionItem from "../../components/question/Item.vue"
 import topbar from "../../components/start/Topbar.vue"
 import exerciseToolbar from "../../components/start/toolbar/Exercise.vue"
 
+import closeIcon from "../../assets/img/icons/close.svg"
+
 export default {
   data() {
     return {
+      closeIcon,
       currentIndex: 0,
-      questionList: []
+      questionList: [],
+      isShowTab: false
     }
   },
   components: {
@@ -58,7 +92,6 @@ export default {
       console.log('event.answer: ', key)
       this.$set(this.currentQuestion, 'selfAnswer', key)
       this.$set(this.currentQuestion, 'isAnswered', true)
-      this.calculateOptionClass(key)
     })
     // 触发事件：收藏
     eventCenter.on('on.collect', status => {
@@ -68,6 +101,11 @@ export default {
         icon: 'none'
       })
       this.$set(this.currentQuestion, 'isCollected', status)
+    })
+    // 触发事件：打开选项卡
+    eventCenter.on('on.show.question.tab', _ => {
+      console.log('event.show.question.tab: ')
+      this.isShowTab = true
     })
     // 触发事件：纠错
     eventCenter.on('on.report.error', _ => {
@@ -146,30 +184,9 @@ export default {
     handleChange(e) {
       this.currentIndex = e.detail.current
     },
-    
-    calculateOptionClass(key) {
-      _.forEach(this.currentQuestion.options, (_, key) => {
-        let classes = ''
-        if (this.checkCorrectOption(key)) {
-          classes = 'bg-green-500 border-green-500'
-        }
-        if (this.checkInCorrectOption(key)) {
-          classes = 'bg-red-500 border-red-500'
-        }
-        this.$set(this.currentQuestion.optionsClasses, key, classes)
-      })
-    },
-    checkCorrectOption(key) {
-      switch(this.questionType) {
-        case 1:
-          return key == this.correctAnswer
-      }
-    },
-    checkInCorrectOption(key) {
-      switch(this.questionType) {
-        case 1:
-          return key == this.answer && key != this.correctAnswer
-      }
+    handleToIndex(index) {
+      this.currentIndex = index
+      this.isShowTab = false
     }
   }
 }
